2cfd4a9736ee06068c7f620ecf7fe1265b0edfcf
vinit-chauhan <chauhanvinit23@gmail.com>
2023-07-26 12:41:22 -0400

    Added text pattern matching with Regex

--- project/src/main/java/ca/uwindsor/acc/processor/Tokenizer.java
+++ project/src/main/java/ca/uwindsor/acc/processor/Tokenizer.java
@@ -11,13 +11,20 @@ public class Tokenizer {
 
     // Add support for Nearest words.
     public static void tokenizer(String text, List<String> words) {
-        String[] tokens = text.split("[< \n.:,]+");
+        String[] tokens = text.split("[ \n]+");
         for (String token : tokens) {
             String word = token.trim();
             if (word.length() > 0 && !IGNORE_WORDS.contains(word)) {
-                words.add(word);
+                // Remove Sensitive Data Such as Phone numbers and Email Addresses.
+                if (isSafeToInsert(word))
+                    words.add(word.replaceAll("[,.:]$", ""));
             }
         }
     }
 
+    public static boolean isSafeToInsert(String text) {
+        return !text.matches("^[a-zA-Z0-9]+([_.-][a-zA-Z-0-9]+)*@[a-zA-Z]+.([a-z]){2,}(.[a-z]+)*$") &&
+                !text.matches("^([+][0-9]{1,3})?[0-9]{10}$") &&
+                !text.matches("[)(*&^%$#@!~.,:]");
+    }
 }
